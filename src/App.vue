939d1b6f6b8b7a20c35e7ede6cd24647dcb09a2c
Pablo <oliveiraapablo@gmail.com>
2024-06-27 15:04:54 -0300

    criando estrutura

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance></Balance>
+    <IncomeExpense></IncomeExpense>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpense from "./components/IncomeExpense.vue";
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpense,
+  },
+};
+</script>
